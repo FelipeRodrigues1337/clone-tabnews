3d79efd4641fb1ca90277b9d3c9895d975cd5156
FelipeRodrigues1337 <proen_dev@hotmail.com>
2025-02-13 08:52:46 -0300

    feat: Configurando prettier

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <div className="container">
-            <h1>Morganinha Morganinha, se você me ama dá uma risadinha 😎</h1>
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1>Morganinha Morganinha, se você me ama dá uma risadinha 😎</h1>
+    </div>
+  );
 }
 
 export default Home;
